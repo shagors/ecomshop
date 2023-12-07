900db57106c4e4db9b1baa864a93065704aac812
thtshajjad <shagors@users.noreply.github.com>
2023-12-07 12:21:39 +0600

    make controller and error handler set

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import productRoutes from "./routes/productRoutes.js";
 import connectDB from "./config/db.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -18,6 +19,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
