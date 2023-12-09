ce4bcc3ab4b1d7b2904cbb8920b20a5ccefcc487
thtshajjad <shagors@users.noreply.github.com>
2023-12-09 12:14:31 +0600

    add products cart and remove from it and auto calculation

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,7 +5,7 @@ import cartSliceReducer from "./slices/cartSlice";
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    cartSliceReducer,
+    cart: cartSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
